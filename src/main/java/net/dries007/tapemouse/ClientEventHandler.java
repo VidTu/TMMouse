91e2e6780234e4001d82b601333463719dab04a1
Matvei Slotvinskiy <42819201+3ako@users.noreply.github.com>
2023-12-08 04:48:50 +0500

    1.20.2

--- src/main/java/net/dries007/tapemouse/ClientEventHandler.java
+++ src/main/java/net/dries007/tapemouse/ClientEventHandler.java
@@ -5,19 +5,19 @@ import java.util.stream.Collectors;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.text.LiteralText;
-import net.minecraft.util.Formatting;
+import net.minecraft.text.Text;
 import ru.vidtu.tapemouse.KeybindArgumentType;
 
 /**
  * Client side only code
- * 
+ *
  * @author Dries007
  * @author VidTu
  * @implNote What do you me by clientside code? Isn't this mod clientside?
@@ -34,30 +34,32 @@ public class ClientEventHandler {
 	 */
 	public static void init() {
 		ClientTickEvents.START_CLIENT_TICK.register(ClientEventHandler::tickEvent);
-		ClientCommandManager.DISPATCHER.register(ClientCommandManager.literal("tapemouse")
-				.executes(c -> {
-					c.getSource().sendFeedback(new LiteralText("TapeMouse help: ").formatted(Formatting.AQUA));
-					c.getSource().sendFeedback(new LiteralText("Run '/tapemouse list' to get a list of keybindings."));
-					c.getSource().sendFeedback(new LiteralText("Run '/tapemouse off' to stop TapeMouse."));
-					c.getSource().sendFeedback(new LiteralText("Run '/tapemouse <binding> <delay>' to start TapeMouse."));
-					c.getSource().sendFeedback(new LiteralText("  delay is the number of ticks between every keypress. Set to 0 to hold down the key."));
-					return 1;
-				})
-				.then(ClientCommandManager.literal("off").executes(c -> {
-					keyBinding = null;
-					return 1;
-				})).then(ClientCommandManager.literal("list").executes(c -> {
-					List<String> keys = KeyBinding.KEYS_BY_ID.keySet().stream().map(k -> k.replaceFirst("^key\\.", ""))
-							.sorted().collect(Collectors.toList());
-					c.getSource().sendFeedback(new LiteralText(String.join(", ", keys)));
-					return keys.size();
-				})).then(ClientCommandManager.argument("key", KeybindArgumentType.INSTANCE)
-						.then(ClientCommandManager.argument("delay", IntegerArgumentType.integer(0)).executes(c -> {
-							ClientEventHandler.delay = c.getArgument("delay", int.class);
-							ClientEventHandler.i = 0;
-							ClientEventHandler.keyBinding = c.getArgument("key", KeyBinding.class);
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(
+				ClientCommandManager.literal("tapemouse")
+						.executes(c -> {
+							c.getSource().sendFeedback(Text.of("TapeMouse help: "));
+							c.getSource().sendFeedback(Text.of("Run '/tapemouse list' to get a list of keybindings."));
+							c.getSource().sendFeedback(Text.of("Run '/tapemouse off' to stop TapeMouse."));
+							c.getSource().sendFeedback(Text.of("Run '/tapemouse <binding> <delay>' to start TapeMouse."));
+							c.getSource().sendFeedback(Text.of("  delay is the number of ticks between every keypress. Set to 0 to hold down the key."));
 							return 1;
-						}))));
+						})
+						.then(ClientCommandManager.literal("off").executes(c -> {
+							keyBinding = null;
+							return 1;
+						})).then(ClientCommandManager.literal("list").executes(c -> {
+							List<String> keys = KeyBinding.KEYS_BY_ID.keySet().stream().map(k -> k.replaceFirst("^key\\.", ""))
+									.sorted().collect(Collectors.toList());
+							c.getSource().sendFeedback(Text.of(String.join(", ", keys)));
+							return keys.size();
+						})).then(ClientCommandManager.argument("key", KeybindArgumentType.INSTANCE)
+								.then(ClientCommandManager.argument("delay", IntegerArgumentType.integer(0)).executes(c -> {
+									ClientEventHandler.delay = c.getArgument("delay", int.class);
+									ClientEventHandler.i = 0;
+									ClientEventHandler.keyBinding = c.getArgument("key", KeyBinding.class);
+									return 1;
+								})))
+		));
 	}
 
 	/**
